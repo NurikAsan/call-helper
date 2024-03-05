a4370e5cab453a5245b980fbf170733dd00c71d0
Nurikov <asankylovnursultan@gmail.com>
2024-03-05 20:27:01 +0600

    Добавлены модели Организация, Группа

---
+++ breaks/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import organisations
+
+@admin.register(organisations.Organisation)
+class OrganisationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'director')
+
+
+@admin.register(organisations.Group)
+class OrganisationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'manager', 'min_active',)
