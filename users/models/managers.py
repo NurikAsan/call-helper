ea87ca2d483fd65ddbb3b4d32cb14ad8b982baa3
Nurikov <asankylovnursultan@gmail.com>
2024-03-08 13:11:49 +0600

    Add Custom manager

--- users/models/managers.py
+++ users/models/managers.py
@@ -1,29 +1,43 @@
 from django.contrib.auth.models import BaseUserManager
+from rest_framework.exceptions import ParseError
 
 
 class CustomUserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, email=None, password=None, username=None, **extra_fields):
+    def _create_user(self, phone_number=None, email=None, password=None, username=None, **extra_fields):
+        if not email or not username or not phone_number:
+            raise ParseError('Укажите email или телефон')
+
         if email:
             email = self.normalize_email(email)
 
+        if not username:
+            if email:
+                username = email.split('@')[0]
+            else:
+                username = phone_number
+
         user = self.model(username=username, **extra_fields)
-        user.email = email
+        if email:
+            user.email = email
+        if phone_number:
+            user.phone_number = phone_number
+
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email=None, password=None, username=None, **extra_fields):
+    def create_user(self,phone_number=None, email=None, password=None, username=None, **extra_fields):
         extra_fields.setdefault('is_superuser', False)
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_active', True)
 
-        return self._create_user(email, password, username, **extra_fields)
+        return self._create_user(phone_number,email, password, username, **extra_fields)
 
-    def create_superuser(self, email=None, password=None, username=None, **extra_fields):
+    def create_superuser(self,phone_number=None, email=None, password=None, username=None, **extra_fields):
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_active', True)
 
-        return self._create_user(email, password, username, **extra_fields)
+        return self._create_user(phone_number,email, password, username, **extra_fields)
--- users/models/users.py
+++ users/models/users.py
@@ -1,9 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
 from .managers import CustomUserManager
+from django.db import models
 
 
 class User(AbstractUser):
-    phone_number = PhoneNumberField(unique=True, null=True)
+    username = models.CharField(max_length=50, unique=True, null=True, blank=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    phone_number = PhoneNumberField(unique=True, null=True, blank=True)
 
     objects = CustomUserManager()
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['email']
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return f'{self.full_name}'
